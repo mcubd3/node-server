8f925a3731f9290a06bd0e9129061cf310346850
zayn <mdrahul6967@gmail.com>
2022-09-16 21:54:28 +0600

    hjh

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.post('/',async (req, res) => {
 var d=await new collec({
   name:req.headers.reqs,
   date:moment().tz('Asia/dhaka').format('h:m a, D/M/YY'),
-  ipad:  req.headers.ip2
+  ipad:  req.headers.reqs
 }).save()
 
   res.send(d) 
