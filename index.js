898fbe4eab913bd2487f5d5a4e6f66b3597e2cf5
zayn <mdrahul6967@gmail.com>
2022-11-30 04:30:21 +0600

    hjh

--- index.js
+++ index.js
@@ -123,7 +123,7 @@ app.post('/ram',async (req, res) => {
     num:await collec.count() +1,
   }).save()
 
-  res.send({...JSON.parse(req.body),num:await collec.count() +1})
+  res.send({...JSON.parse(req.body),num:await collec.count()})
 
 })
 
