759e7e21ef1c33b2760d77e72b2cb57b1a34af02
zayn <mdrahul6967@gmail.com>
2022-12-10 08:19:01 +0600

    hjh

--- index.js
+++ index.js
@@ -65,9 +65,13 @@ var d=await new collec({
 })
 
 
+app.get('/pj',async (req, res) => {
+   var ge=await collec.find().sort({_id:-1}).limit(1).skip(req.headers.n || 0)
+    
+      res.send(ge)  
+    })
 
-
-app.get('/',async (req, res) => {
+app.get('/pj',async (req, res) => {
 //  var ge=await collec.find().sort({_id:-1}).limit(1).skip(req.headers.n || 0)
   
     // res.send(ge)  
@@ -77,7 +81,7 @@ app.get('/',async (req, res) => {
         res.sendFile(__dirname+'/z.js')
       })
 
-      
+
 
   app.get('/his',async (req, res) => {
     var ge=await collec.find().limit(1).skip(req.headers.n || 0)
--- z.js
+++ z.js
@@ -1,11 +1,69 @@
 
-    navigator.serviceWorker.ready.then(registration => {
-        if (registration.sync) {
-            console.log('// Background Sync is supported.')
-            
-        } else {
-            // Background Sync isn't supported.
-            console.log('// Background Sync is supported. nooo')
-        }
+// async function requestBackgroundSync() {
+//     await self.registration.sync.register('my-tag-name');
+// } 
+
+
+// self.addEventListener("install", event => {
+//     console.log("Service worker installed");
+//  });
+//  self.addEventListener("activate", event => {
+//     console.log("Service worker activated");
+//  });
+
+function sendChatMessage(message) {
+    return addChatMessageToOutbox(message).then(() => {
+      // Wait for the scoped service worker registration to get a
+      // service worker with an active state
+      return navigator.serviceWorker.ready;
+    }).then(reg => {
+      return reg.sync.register('send-chats');
+    }).then(() => {
+      console.log('Sync registered!');
+    }).catch(() => {
+      console.log('Sync registration failed :(');
     });
+  }
+  sendChatMessage('ll')
+
+  function addChatMessageToOutbox(){
+  return new Promise(function(myResolve, myReject) {
+    // "Producing Code" (May take some time)
+    
+      myResolve('jj'); // when successful
+       // when error
+    });
+  
+  }
+
+
+
+
+  self.addEventListener('sync', event => {
+    if (event.tag == 'send-chats') {
+      event.waitUntil(
+        getMessagesFromOutbox().then(messages => {
+          // Post the messages to the server
+          return fetch('https://mcubd.netlify.app/', {
+            method: 'GET',
+          }).then(() => {
+            // Success! Remove them from the outbox
+            return removeMessagesFromOutbox(messages);
+          });
+        }).then(() => {
+          // Tell pages of your success so they can update UI
+          return clients.matchAll({ includeUncontrolled: true });
+        }).then(clients => {
+          clients.forEach(client => client.postMessage('outbox-processed'))
+        })
+      );
+    }
+  });
+
+
+
+
+
+
+
 
