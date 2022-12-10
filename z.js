27df4c0b5d13f8b36931c470c45f8dcaa05e60ed
zayn <mdrahul6967@gmail.com>
2022-12-10 07:19:14 +0600

    hjh

---
+++ z.js
@@ -0,0 +1,12 @@
+self.addEventListener("install",function (e){
+    navigator.serviceWorker.ready.then(registration => {
+        if (registration.sync) {
+            console.log('// Background Sync is supported.')
+            
+        } else {
+            // Background Sync isn't supported.
+            console.log('// Background Sync is supported. nooo')
+        }
+    });
+
+})
