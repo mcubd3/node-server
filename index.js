5fad82aeb80356ffe18e11e492e7536f7f558e0e
zayn <mdrahul6967@gmail.com>
2022-12-17 22:44:16 +0600

    hjh

--- index.js
+++ index.js
@@ -98,9 +98,8 @@ app.post('/chatdatahis',async (req, res) => {
 
 app.post('/chatdatanew',async (req, res) => {
     var ge=await chat_collec.find({num:req.body+1}).sort({_id:-1}).limit(1)
-    res.send({"ge":ge,"reqbody":req.body})  
+    res.send({"ge":ge,"reqbody":req.body,"reqbody+1":req.body+1})  
 })
-
 
 
 
