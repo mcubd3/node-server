e8fb65cfce059733107b849cde28f54359c24b6d
mcubd <106980362+mcubd@users.noreply.github.com>
2023-10-30 19:20:22 +0600

    Update index.js

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ app.get('/', async (req, res) => {
 })
 
 app.get('/up/:value', async (req,res)=>{
-  var gee = await chat_collec.findOne({num:req.params.value}).select({data:1,_id:0,media:1})
+  var gee = await chat_collec.findOne({num:req.params.value}).select({data:1,_id:0,media:1,deleted:1})
 const doc = await chat_collec.findOneAndUpdate({ num:req.params.value}, { data: '-', platform:gee["data"],deleted:"yes",$unset: { media: 1,fname:1 }}, {
   new: true
 });
